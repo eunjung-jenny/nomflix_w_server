bf852a077c43642badd861de78bab04a3bc14970
jenny.kr <jenny.it.kr@gmail.com>
2020-05-29 15:19:54 +0900

    배포용으로 api 요청 url 변경

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://13.209.135.218/api/",
+  baseURL:
+    "http://first-load-balancer-603173088.ap-northeast-2.elb.amazonaws.com/api/",
 });
 
 export const moviesApi = {
